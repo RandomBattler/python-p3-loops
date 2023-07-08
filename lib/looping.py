1adbf5d75239de3708f28d581b70e01b5e56d3db
Steve Romero <romero.steve.a@gmail.com>
2023-07-07 18:25:01 -0700

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+
+    while count > 0:
+        print(count)
+        count-=1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    sqInts = list()
+    for i in int_list:
+        sqInts.append(i*i)
+    
+    return sqInts
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 1
+    while count <= 100:
+        txt = ""
+        if (count%3 == 0):
+            txt += "Fizz"
+        if (count%5 == 0):
+            txt += "Buzz"
+        
+        if(txt == ""):
+            print(count)
+        else:
+            print(txt)
+
+        count+=1
+
+        
+
+        
